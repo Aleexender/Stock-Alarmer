7563a0c83f2ff7aa8a5bbcb7ec25b55821a844bc
Aleexender <alexkjh2011@gmail.com>
2025-01-07 19:29:04 +0900

    fix: config

--- src/main/java/org/example/stock_alarmer/infra/stockApi/webclient/config/AlphaVantageConfig.java
+++ src/main/java/org/example/stock_alarmer/infra/stockApi/webclient/config/AlphaVantageConfig.java
@@ -8,17 +8,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @Getter
 @ConfigurationProperties(prefix = "alphavantage")
  public class AlphaVantageConfig {
-    @Value("${alphavantage.api-key}")
-    private final String apiKey;
-    @Value("${alphavantage.baseurl}")
-    private final String baseurl;
-    private final DateFormatter dateFormatter;
-
-    protected AlphaVantageConfig(String apiKey, String baseurl, DateFormatter dateFormatter) {
-        this.apiKey = apiKey;
-        this.baseurl = baseurl;
-        this.dateFormatter = dateFormatter;
-    }
+    private String apiKey;
+    private String baseurl;
+    private DateFormatter dateFormatter;
 
     public String getCurrentFormattedDate() {
         return dateFormatter.formatCurrentDate();
