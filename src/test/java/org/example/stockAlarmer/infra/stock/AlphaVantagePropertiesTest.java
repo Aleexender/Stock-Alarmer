bd37323ce47753eb24483a4fe900e6899725fd6d
Aleexender <alexkjh2011@gmail.com>
2025-02-06 21:43:27 +0900

    fix: fix properties and move directory

--- src/test/java/org/example/stockAlarmer/infra/stock/AlphaVantagePropertiesTest.java
+++ src/test/java/org/example/stockAlarmer/infra/stock/AlphaVantagePropertiesTest.java
@@ -9,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
-@TestPropertySource(locations = "classpath:application-test.properties")
+@TestPropertySource
 class AlphaVantagePropertiesTest {
     @Autowired
     private AlphaVantageProperties alphaVantageConfig;
