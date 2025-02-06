8138f21e6d138e7b84be01144ae22634d4720297
Aleexender <alexkjh2011@gmail.com>
2025-02-06 21:50:46 +0900

    fix : alarmController

--- src/main/java/org/example/stockAlarmer/module/alarm/presentation/AlarmController.java
+++ src/main/java/org/example/stockAlarmer/module/alarm/presentation/AlarmController.java
@@ -1,15 +1,8 @@
 package org.example.stockAlarmer.module.alarm.presentation;
 
 import lombok.RequiredArgsConstructor;
-import org.example.stockAlarmer.global.common.ApiResponse;
 import org.example.stockAlarmer.module.alarm.application.AlarmService;
-import org.example.stockAlarmer.module.alarm.dto.AlarmDto;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
-import static org.springframework.http.HttpStatus.CREATED;
 
 
 @RestController
@@ -17,11 +10,11 @@ import static org.springframework.http.HttpStatus.CREATED;
 public class AlarmController {
     private final AlarmService alarmService;
 
-    @PostMapping
-    @ResponseStatus(CREATED)
-    public ApiResponse subscribeAlarm(@RequestBody AlarmDto.SubscribeDto request){
-        alarmService.subscribe(request);
-    }
+//    @PostMapping
+//    @ResponseStatus(CREATED)
+//    public ApiResponse subscribeAlarm(@RequestBody AlarmDto.SubscribeDto request){
+//        alarmService.subscribe(request);
+//    }
 
 
 
