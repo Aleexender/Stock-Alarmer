dc0c567150cb65cd48e4c3fb5e2caffc1e03e9ac
Aleexender <alexkjh2011@gmail.com>
2025-02-19 02:02:54 +0900

    feat: make api

--- src/main/java/org/example/stockAlarmer/module/alarm/presentation/AlarmController.java
+++ src/main/java/org/example/stockAlarmer/module/alarm/presentation/AlarmController.java
@@ -1,10 +1,13 @@
 package org.example.stockAlarmer.module.alarm.presentation;
 
 import lombok.RequiredArgsConstructor;
+import org.example.stockAlarmer.global.general.ApiResponse;
 import org.example.stockAlarmer.module.alarm.application.AlarmService;
 import org.example.stockAlarmer.module.alarm.dto.AlarmDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -17,5 +20,12 @@ public class AlarmController {
     @ResponseStatus(HttpStatus.CREATED)
     public void subscribeAlarm(@RequestBody AlarmDto.SubscribeDto request){
         alarmService.subscribe(request);
+    }
+
+    @GetMapping("/{email}")
+    @ResponseStatus(HttpStatus.OK)
+    public ApiResponse<List<AlarmDto.GetSubscribeDto>> getAlarms(@PathVariable String email){
+        List<AlarmDto.GetSubscribeDto> alarms = alarmService.getAlarms(email);
+        return ApiResponse.ok(alarms);
     }
 }
