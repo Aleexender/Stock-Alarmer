a4f13e317cf058cf8b5468787cb89138af4c2753
Aleexender <alexkjh2011@gmail.com>
2025-02-19 02:01:53 +0900

    feat: add DTOs

--- src/main/java/org/example/stockAlarmer/module/alarm/dto/AlarmDto.java
+++ src/main/java/org/example/stockAlarmer/module/alarm/dto/AlarmDto.java
@@ -3,7 +3,7 @@ package org.example.stockAlarmer.module.alarm.dto;
 import org.jetbrains.annotations.NotNull;
 
 import static org.example.stockAlarmer.module.alarm.dto.AlarmDto.SubscribeDto;
-public sealed interface AlarmDto permits SubscribeDto {
+public sealed interface AlarmDto permits SubscribeDto, AlarmDto.GetSubscribeDto {
     record SubscribeDto(
             String name,
             @NotNull
@@ -13,6 +13,16 @@ public sealed interface AlarmDto permits SubscribeDto {
             @NotNull
             Double price,
             String messengerType
+    ) implements AlarmDto {
+
+    }
+
+    record GetSubscribeDto(
+            String name,
+            String symbol,
+            String email,
+            Double price
+
     ) implements AlarmDto {
 
     }
