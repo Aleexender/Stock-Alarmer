29468cc1c8e9502dd8e77414f7b531f291a47798
Aleexender <alexkjh2011@gmail.com>
2025-02-21 23:00:03 +0900

    feat: add new infa api

--- src/main/java/org/example/stockAlarmer/infra/stock/AlphaVantageStockApi.java
+++ src/main/java/org/example/stockAlarmer/infra/stock/AlphaVantageStockApi.java
@@ -3,18 +3,20 @@ package org.example.stockAlarmer.infra.stock;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.stockAlarmer.global.date.DateFormatter;
+import org.example.stockAlarmer.infra.stock.dto.StockInfraDto;
 import org.example.stockAlarmer.module.stock.application.StockApi;
 import org.example.stockAlarmer.module.stock.domain.Stock;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 class AlphaVantageStockApi implements StockApi {
     private final WebClient webClient;
-    private final AlphaVantageProperties alphaVantageConfig;
+    private final AlphaVantageProperties properties;
     private final DateFormatter dateFormatter;
     private final ResponseConverter responseConverter;
 
@@ -24,7 +26,7 @@ class AlphaVantageStockApi implements StockApi {
                 .uri(uriBuilder -> uriBuilder
                         .path("/query")
                         .queryParam("function", "LISTING_STATUS")
-                        .queryParam("apikey", alphaVantageConfig.getApiKey())
+                        .queryParam("apikey", properties.getApiKey())
                         .queryParam("state", "active")
                         .queryParam("date", dateFormatter.CurrentDate())
                         .build())
@@ -33,5 +35,18 @@ class AlphaVantageStockApi implements StockApi {
                 .map(responseConverter::parse)
                 .flatMapMany(Flux::fromIterable)
                 .doOnNext(stock -> log.info("Stock processed: {}", stock));
+    }
+
+    @Override
+    public Mono<StockInfraDto.StockDetailResponseWrapper> getDetails(String symbol) {
+        return webClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/query")
+                        .queryParam("function", "GLOBAL_QUOTE")
+                        .queryParam("symbol", symbol)
+                        .queryParam("apikey", properties.getApiKey())
+                        .build())
+                .retrieve()
+                .bodyToMono(StockInfraDto.StockDetailResponseWrapper.class);
     }
 }
